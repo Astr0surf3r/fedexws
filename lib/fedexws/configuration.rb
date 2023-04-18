2cd2a1b732b62616b0306142cd1b7e63867e447c
salmital <salvatore.italiano@sterlingcapitalbrokers.com>
2023-04-17 19:34:34 -0700

    retrieve FedEx credential params from initializer file

--- lib/fedexws/configuration.rb
+++ lib/fedexws/configuration.rb
@@ -12,10 +12,10 @@ module Fedexws
     def load_parameters
       initializer_file = Rails.root.join('config', 'initializers', 'fedexws.rb')
       config = File.read(initializer_file)
-      @api_key = config.match(/api_key\s*=\s*['"]([^'"]+)['"]/)&.captures&.first
-      @api_secret = config.match(/api_secret\s*=\s*['"]([^'"]+)['"]/)&.captures&.first
-      @api_account_number = config.match(/api_account_number\s*=\s*['"]([^'"]+)['"]/)&.captures&.first
-      @api_meter_number = config.match(/api_meter_number\s*=\s*['"]([^'"]+)['"]/)&.captures&.first
+      @api_key = ENV["#{config.match(/config\.api_key\s*=\s*ENV\['([^']+)'\]/)&.captures.first}"]
+      @api_secret = ENV["#{config.match(/config\.api_secret\s*=\s*ENV\['([^']+)'\]/)&.captures.first}"]
+      @api_account_number = ENV["#{config.match(/config\.api_account_number\s*=\s*ENV\['([^']+)'\]/)&.captures.first}"]
+      @api_meter_number = ENV["#{config.match(/config\.api_meter_number\s*=\s*ENV\['([^']+)'\]/)&.captures.first}"]
       @credentials = [@api_key, @api_secret, @api_account_number, @api_meter_number]
     end
   end
